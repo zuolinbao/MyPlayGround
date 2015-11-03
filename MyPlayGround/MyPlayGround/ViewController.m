62166326d111988aa44b6832d988d87ef5e03732
zuolinbao <zuolinbao@icloud.com>
2015-11-03 19:52:17 +0800

    YYImage 三方库

--- MyPlayGround/MyPlayGround/ViewController.m
+++ MyPlayGround/MyPlayGround/ViewController.m
@@ -10,53 +10,91 @@
 #import "LBOneViewController.h"
 #import "LBTwoViewController.h"
 
+
+
+static NSInteger kWidth = 10;
 @interface ViewController ()
 @property (nonatomic,strong)UIViewController *showingVC;
-@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (nonatomic,strong)UILabel *label;
+@property (nonatomic,strong)UIScrollView *scrollView;
+@property (nonatomic,strong)UIView *containerView;
 @end
 
 @implementation ViewController
+- (IBAction)update:(id)sender {
+
+    kWidth -=10;
+    [self.label mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.width.equalTo(@(kWidth));
+    }];
+   
+    NSLog(@"%@",NSStringFromCGRect(self.label.frame));
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     //NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"TestJSONData.json" ofType:nil]] options:NSJSONReadingMutableLeaves error:nil];
-    LBOneViewController *oneVC =[[LBOneViewController alloc] init];
-    LBTwoViewController *towVC = [[LBTwoViewController alloc] init];
-    [self addChildViewController:oneVC];
-    [self addChildViewController:towVC];
-
+    
+    self.scrollView = [UIScrollView new];
+    self.scrollView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:self.scrollView];
+    [self.scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+       make.edges.equalTo(self.view).insets(UIEdgeInsetsMake(100, 10, 10, 10));
+    }];
+    
+    _containerView = [UIView new];
+    _containerView.backgroundColor = [UIColor purpleColor];
+    [self.scrollView addSubview:_containerView];
+    [_containerView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.scrollView).insets(UIEdgeInsetsMake(10, 10, 10, 10));
+        make.width.equalTo(self.scrollView).offset(-20);
+    }];
+    
+    
+     _label = [UILabel new];
+    [self.containerView addSubview:_label];
+    _label.text =@"dhfjkshdjfhsjdfhkjshdf";
+    _label.numberOfLines = 0;
+    [_label mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(_containerView).offset(10);
+        make.top.equalTo(_containerView);
+        //make.edges.equalTo(self.scrollView).insets(UIEdgeInsetsMake(10, 10, 10, 10));
+        make.width.equalTo(@80);
+    }];
+    
+    
+    UIView *redView = [UIView new];
+    redView.backgroundColor = [UIColor redColor];
+    [self.containerView addSubview:redView];
+//    [redView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.size.mas_equalTo(CGSizeMake(50, 50));
+//        make.top.equalTo(self.label).offset(800);
+//        make.left.equalTo(self.label).offset(50);
+//        make.bottom.equalTo(self.containerView);
+//    }];
+    [redView.leftAnchor constraintEqualToAnchor:_label.leftAnchor constant:10];
+    [redView.topAnchor constraintEqualToAnchor:_label.bottomAnchor constant:20];
+    [redView.widthAnchor constraintEqualToConstant:100];
+    [redView.heightAnchor constraintEqualToConstant:100];
     
 
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-//    [_showingVC.view removeFromSuperview];
-//    if ([_showingVC isKindOfClass:[LBOneViewController class]]) {
-//        _showingVC = self.childViewControllers[1];
-//       
-//    }else{
-//        _showingVC = self.childViewControllers[0];
-//    }
-//    [_containerView addSubview:_showingVC.view];
-    //[self.view1 removeFromSuperview];
-
-    UIView *twoView = [[UIView alloc] initWithFrame:self.view.bounds];
-    twoView.backgroundColor = [UIColor blueColor];
-    [self.view addSubview:twoView];
-    CATransition *transition = [CATransition animation];
-    transition.duration = 0.25;
-    transition.type = @"cube";
-    transition.subtype = kCATransitionPush;
-    [self.view.layer addAnimation:transition forKey:nil];
     
-    
-    
+    kWidth +=10;
+        [self.label mas_updateConstraints:^(MASConstraintMaker *make) {
+            make.width.equalTo(@(kWidth));
+        }];
+    [UIView animateWithDuration:4 animations:^{
+        //[self.view layoutIfNeeded];
+        //[self.containerView updateConstraints];
+        [self.containerView setNeedsLayout];
+    }];
+   //NSLog(@"%@",NSStringFromCGRect(self.label.frame));
 }
 
--(void)transitionFromViewController:(UIViewController *)fromViewController toViewController:(UIViewController *)toViewController duration:(NSTimeInterval)duration options:(UIViewAnimationOptions)options animations:(void (^)(void))animations completion:(void (^)(BOOL))completion{
 
-
-}
 
 @end
